1e5fb9e0e9ec8a6ede49e7f044fdcaa158ab4f6a
Alan Coleman <acoleman@maxisistemas.com.ar>
2022-08-26 21:37:14 -0300

    practico estructurada

---
+++ ejercicio8/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Ejercicio8
+{
+    private static void Main(string[] args)
+    {
+        int numero1 = 10;
+        int numero2 = 25;
+
+        int producto = numero1 * numero2;
+
+        System.Console.WriteLine("El producto de los numero1 y numero2 es : " + producto);
+
+    }
+}
